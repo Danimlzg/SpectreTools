23dd5c69032c4128aa04f017276bc60ab98275cd
Danimlzg <danimlzg@gmail.com>
2026-02-17 22:21:25 +0100

    Mover __init__.py a su ubicación correcta en tools/

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,15 +1,3 @@
-from .smart_fuzzer import SmartFuzzer
-from .fast_port_scanner import AsyncPortScanner
 from .ultra_fast_scanner import UltraFastScanner
-from .waf_bypasser import WAFBypasser
-from .ultra_dns_enum import UltraDNSEnum
-from .vuln_scanner import VulnScanner
 
-__all__ = [
-    'SmartFuzzer',
-    'AsyncPortScanner',
-    'UltraFastScanner',
-    'WAFBypasser',
-    'UltraDNSEnum',
-    'VulnScanner'
-]
+__all__.append('UltraFastScanner')
